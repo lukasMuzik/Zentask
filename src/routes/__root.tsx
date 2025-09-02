77d0b72ab3f77321fcd3da64757d30faa45a6d99
Lukáš Mužík <lukasmuzik.dev@gmail.com>
2025-09-02 19:37:33 +0200

    feat: simplify root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,9 @@
-import {createRootRoute, Link, Outlet} from '@tanstack/react-router';
+import {createRootRoute, Outlet} from '@tanstack/react-router';
 import {TanStackRouterDevtools} from '@tanstack/react-router-devtools';
-import {Flex} from '@chakra-ui/react';
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Flex gap="4px" alignItems="center">
-        <Link to="/">Home</Link> <Link to="/about">About</Link>
-      </Flex>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
