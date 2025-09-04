3dc08f48ca3c0d32f624e8e0f1b6762e7553ef74
Lukáš Mužík <lukasmuzik.dev@gmail.com>
2025-09-04 08:27:24 +0200

    fix: /api/login route

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -39,18 +39,19 @@ userRoutes.post('/api/register', (req: Request, res: Response) => {
 userRoutes.post('/api/login', (req: Request, res: Response) => {
   const {password, username} = validateCredentialsFromBody(req, res);
 
-  userDB.findOne(
-    (user: User) => user.username === username && comparePassword(password, user.password),
-    (err: Error | null, user: User | null) => {
-      if (isNotNil(err)) return res.status(500).json({error: 'Internal server error'});
-      if (isNil(user)) return res.status(401).json({error: 'Invalid credential'});
+  userDB.findOne({username}, (err: Error | null, user: User | null) => {
+    if (isNotNil(err)) return res.status(500).json({error: 'Internal server error'});
+    if (isNil(user)) return res.status(401).json({error: 'User not found'});
 
-      const accessToken = signAccessToken(user.id, username);
-      const refreshToken = signRefreshToken(user.id, username);
-
-      res.status(200).json({accessToken, refreshToken});
+    if (!comparePassword(password, user.password)) {
+      return res.status(401).json({error: 'Invalid credential'});
     }
-  );
+
+    const accessToken = signAccessToken(user.id, username);
+    const refreshToken = signRefreshToken(user.id, username);
+
+    res.status(200).json({accessToken, refreshToken});
+  });
 });
 
 userRoutes.post('/api/refresh-token', (req: Request, res: Response) => {
