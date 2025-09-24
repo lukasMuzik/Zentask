d30333d6069edf93c5caed5b8294bfea7db8399d
Lukáš Mužík <lukasmuzik.dev@gmail.com>
2025-09-24 18:01:54 +0200

    feat: add Error

---
+++ src/shared/ui/Error/Error.tsx
@@ -0,0 +1,36 @@
+import {Center, VStack, Text, HStack} from '@chakra-ui/react';
+import {Button} from '@ui/Button';
+import {useNavigate} from '@tanstack/react-router';
+
+interface ErrorHandlerProps {
+  message?: string;
+  onRetry?: () => void;
+  action?: {
+    label: string;
+    onClick: () => void;
+  };
+}
+
+export function Error({message = 'Unexpected error', action}: ErrorHandlerProps) {
+  const navigate = useNavigate();
+
+  return (
+    <Center>
+      <VStack spacing="1rem">
+        <Text color="red.500" textAlign="center">
+          {message}
+        </Text>
+        <HStack>
+          <Button onClick={() => navigate({to: '/'})} variant="secondary">
+            Back to task list
+          </Button>
+          {action && (
+            <Button onClick={() => action.onClick()} variant="secondary">
+              {action.label}
+            </Button>
+          )}
+        </HStack>
+      </VStack>
+    </Center>
+  );
+}
